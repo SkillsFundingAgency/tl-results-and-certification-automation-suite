636ef9665521035b472154cff444599c3d40a03c
rajchala <rajchala@gmail.com>
2020-04-20 15:39:22 +0100

    Removed one of the DfE Signin test

--- src/Sfa.Tl.ResultsAndCertificationAutomation/Tests/StepDefinations/DfESignInInvalidUserSteps.cs
+++ src/Sfa.Tl.ResultsAndCertificationAutomation/Tests/StepDefinations/DfESignInInvalidUserSteps.cs
@@ -9,13 +9,26 @@ namespace Sfa.Tl.ResultsAndCertificationAutomation.Tests.StepDefinations
     [Binding]
     public class DfESignInInvalidUserSteps : DfESignInPage
     {
+        [Given(@"TLevel start page")]
+        public void GivenTLevelStartPage()
+        {
+            WebDriver.Navigate().GoToUrl(StartPage.StartPageUrl);
+        }
+
         [Given(@"I have entered invalid Username or Password")]
         public void GivenIHaveEnteredInvalidUsernameOrPassword()
         {
+            WebDriver.FindElement(StartNowButton).Click();
             WebDriver.FindElement(UserIdTxtBox).SendKeys(DfeInvalidUserName);
             WebDriver.FindElement(PasswordTxtBox).SendKeys(DfeInvalidPassword);
         }
-        
+
+        [When(@"I clicked on sign in button")]
+        public void WhenIClickedOnSignInButton()
+        {
+            WebDriver.FindElement(SignInButton).Click();
+        }
+
         [Then(@"User should not logged to see Tlevel Dashboard page")]
         public void ThenUserShouldNotLoggedToSeeTlevelDashboardPage()
         {
