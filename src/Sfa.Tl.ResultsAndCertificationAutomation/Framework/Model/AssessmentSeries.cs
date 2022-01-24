1343b43fbdaedecf2b8784dd512733f35c8aea0f
rajchala <rajchala@gmail.com>
2022-01-24 10:21:39 +0000

    Turned off Headless browser

---
+++ src/Sfa.Tl.ResultsAndCertificationAutomation/Framework/Model/AssessmentSeries.cs
@@ -0,0 +1,14 @@
+﻿namespace Sfa.Tl.ResultsAndCertificationAutomation.Framework.Model
+{
+    public enum AssessmentSeries
+    {
+        Summer2021 = 1,
+        Autumn2021 = 2,
+        Summer2022 = 3,
+        Autumn2022 = 4,
+        Summer2023 = 5,
+        Autumn2023 = 6,
+        Summer2024 = 7,
+        Autumn2024 = 8
+    }
+}
