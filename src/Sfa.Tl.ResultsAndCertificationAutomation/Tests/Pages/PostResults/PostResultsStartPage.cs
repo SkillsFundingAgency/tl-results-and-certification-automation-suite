b48e3884f66c58494878440da8cf24f3ea9ee220
rajchala <rajchala@gmail.com>
2022-03-10 16:59:53 +0000

    5745: Post results learner withdrawn page

--- src/Sfa.Tl.ResultsAndCertificationAutomation/Tests/Pages/PostResults/PostResultsStartPage.cs
+++ src/Sfa.Tl.ResultsAndCertificationAutomation/Tests/Pages/PostResults/PostResultsStartPage.cs
@@ -2,29 +2,28 @@
 using OpenQA.Selenium;
 using Sfa.Tl.ResultsAndCertificationAutomation.Framework.Helpers;
 
-namespace Sfa.Tl.ResultsAndCertificationAutomation.Tests.Pages.PostResults
+namespace Sfa.Tl.ResultsAndCertificationAutomation.Tests.Pages.PostResults;
+
+public class PostResultsStartPage : ElementHelper
 {
-    public class PostResultsStartPage : ElementHelper
+    private static string PageUrl { get; } = string.Concat(StartPage.StartPageUrl, "post-results-reviews-appeals-and-grade-changes");
+    private static string PageTitle { get; } = "Post-results reviews, appeals and grade changes – Manage T Level results – GOV.UK";
+    private static string PageHeader { get; } = "Post-results reviews, appeals and grade changes";
+    private static readonly By PageHeaderElement = By.XPath("//*[@id='main-content']//h1");
+    private static By ContinueBtn { get; } = By.Id("continueButton");
+    private static By HomeBreadcrumb { get; } = By.Id("breadcrumb0");
+
+    public static void VerifyReviewAndAppealsStartPage()
     {
-        private static string PageUrl { get; } = string.Concat(StartPage.StartPageUrl, "post-results-reviews-appeals-and-grade-changes");
-        private static string PageTitle { get; } = "Post-results reviews, appeals and grade changes – Manage T Level results – GOV.UK";
-        private static string PageHeader { get; } = "Post-results reviews, appeals and grade changes";
-        private static readonly By PageHeaderElement = By.XPath("//*[@id='main-content']//h1");
-        private static By ContinueBtn { get; } = By.Id("continueButton");
-        private static By HomeBreadcrumb { get; } = By.Id("breadcrumb0");
-
-        public static void VerifyReviewAndAppealsStartPage()
-        {
-            Assert.AreEqual(PageUrl, WebDriver.Url);
-            Assert.AreEqual(PageTitle, WebDriver.Title);
-            Assert.AreEqual(PageHeader, WebDriver.FindElement(PageHeaderElement).Text);
-        }
-
-        protected static void ClickHomeBreadcrumb()
-        {
-            ClickElement(HomeBreadcrumb);
-            TlevelDashboardPage.VerifyDashboardpage();
-        }
-        public static void PressContinueBtn() => ClickButton(ContinueBtn);
+        Assert.AreEqual(PageUrl, WebDriver.Url);
+        Assert.AreEqual(PageTitle, WebDriver.Title);
+        Assert.AreEqual(PageHeader, WebDriver.FindElement(PageHeaderElement).Text);
     }
+
+    protected static void ClickHomeBreadcrumb()
+    {
+        ClickElement(HomeBreadcrumb);
+        TlevelDashboardPage.VerifyDashboardpage();
+    }
+    public static void PressContinueBtn() => ClickButton(ContinueBtn);
 }
